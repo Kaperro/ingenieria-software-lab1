73e06df25428ff24ef0abf7d3428c81a0d1dcbe5
Kaperro <emendezc2@miumg.edu.gt>
2019-04-21 13:54:16 -0500

    error

---
+++ laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/model/Error.java
@@ -0,0 +1,30 @@
+package gt.edu.umg.ingenieria.sistemas.laboratorio1.model;
+
+import java.io.Serializable;
+
+public class Error {
+    private String name;
+    private String message;
+
+    public Error(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
