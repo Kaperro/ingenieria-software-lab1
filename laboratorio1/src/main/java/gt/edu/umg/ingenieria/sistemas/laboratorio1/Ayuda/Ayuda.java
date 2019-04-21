a9ca2d9cad8e812a98f17d0f098f860dbe7ea6f1
Kaperro <emendezc2@miumg.edu.gt>
2019-04-21 13:57:20 -0500

    Ayuda

--- laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/Ayuda/Ayuda.java
+++ laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/Ayuda/Ayuda.java
@@ -1,4 +1,28 @@
 package gt.edu.umg.ingenieria.sistemas.laboratorio1.Ayuda;
+import java.util.Calendar;
+import java.util.Locale;
+import static java.util.Calendar.*;
+import java.util.Date;
 
 public class Ayuda {
+    public static boolean nAnios(Date birtDate, int N) {
+    Calendar calendar = getCalendar(new Date());
+    Calendar calendar1 = getCalendar(birtDate);
+    int Diferencia = calendar.get(YEAR) - calendar1.get(YEAR);
+    if
+    (
+            calendar1.get(MONTH) > calendar.get(MONTH) ||
+                    (calendar1.get(MONTH) == calendar.get(MONTH) && calendar1.get(DATE) > calendar.get(DATE))
+    )
+    {
+        Diferencia--;
+    }
+    return Diferencia >= N;
+}
+
+    private static Calendar getCalendar(Date date) {
+        Calendar cal = Calendar.getInstance(Locale.US);
+        cal.setTime(date);
+        return cal;
+    }
 }
